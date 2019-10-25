a0553f214aeed0bf8813eb8942fb4ee860f64da2
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-24 23:38:13 -0500

    Se hizo Componente de camara con image-picker

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, View, Image, Button } from 'react-native';
+
+import ImagePicker from 'react-native-image-picker';
 
 const styles = StyleSheet.create({
     container: {
@@ -10,16 +12,56 @@ const styles = StyleSheet.create({
     }
 });
 
-class App extends Component {
-    selectImagePicker = () => {
+const options = {
+    title: 'Seleccionar foto',
+    // customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+    // storageOptions: {
+    //     skipBackup: true,
+    //     path: 'images',
+    // },
+    noData: true,
+    mediaType: 'photo'
+};
 
+class App extends Component {
+
+    state = {
+        avatarSource: null
     }
+
+    selectImage = async () => {
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            } else {
+                //const source = { uri: response.uri };
+
+                // You can also display the image using data:
+                const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+                this.setState({
+                    avatarSource: source,
+                });
+            }
+        });
+    }
+
     render() {
-        return(
+        return (
             <View styles={styles.container}>
+                {
+                    this.state.avatarSource &&
+                    <Image style={{ width: '80%', height: 200, resizeMode: 'contain' }} />
+                }
                 <Button
                     title="Seleccione foto"
-                    onPress={this.selectImagePicker}
+                    onPress={this.selectImage}
                 />
             </View>
         );
