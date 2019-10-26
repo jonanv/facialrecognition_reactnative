15ece657cddb7622aef737cdba930bdd58a62f70
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-26 15:33:50 -0500

    Funcionamiento en iOS sin importar image picker

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Button, Image } from 'react-native';
 
-import ImagePicker from 'react-native-image-picker';
+// import ImagePicker from 'react-native-image-picker';
 
 const styles = StyleSheet.create({
     container: {
@@ -31,30 +31,31 @@ class App extends Component {
         }
     }
 
-    selectImage = () => {
-        ImagePicker.showImagePicker(options, (response) => {
-            console.log('Response = ', response);
+    selectImage = async() => {
+        console.log('Preciono la función selectImage');
+        // ImagePicker.showImagePicker(options, (response) => {
+        //     console.log('Response = ', response);
 
-            if (response.didCancel) {
-                console.log('User cancelled image picker');
-            } else if (response.error) {
-                console.log('ImagePicker Error: ', response.error);
-            } else if (response.customButton) {
-                console.log('User tapped custom button: ', response.customButton);
-            } else {
-                //const source = { uri: response.uri };
+        //     if (response.didCancel) {
+        //         console.log('User cancelled image picker');
+        //     } else if (response.error) {
+        //         console.log('ImagePicker Error: ', response.error);
+        //     } else if (response.customButton) {
+        //         console.log('User tapped custom button: ', response.customButton);
+        //     } else {
+        //         //const source = { uri: response.uri };
 
-                // You can also display the image using data:
-                // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+        //         // You can also display the image using data:
+        //         // const source = { uri: 'data:image/jpeg;base64,' + response.data };
 
-                // Se guarda la imagen y se formtea para base64
-                const base64img = 'data:image/jpeg;base64,' + response.data;
+        //         // Se guarda la imagen y se formtea para base64
+        //         const base64img = 'data:image/jpeg;base64,' + response.data;
 
-                this.setState({
-                    avatarSource: response.uri,
-                });
-            }
-        });
+        //         this.setState({
+        //             avatarSource: response.uri,
+        //         });
+        //     }
+        // });
     }
 
     render() {
@@ -66,7 +67,7 @@ class App extends Component {
                 }
                 <Button
                     title='Seleccione imagen'
-                // onPress={this.selectImage}
+                    onPress={this.selectImage}
                 />
             </View>
         );
