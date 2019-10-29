86766e6a310ea495b83138c2fb855771f10a23fd
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-28 22:10:37 -0500

    Se creo el provider de de facialrecognition

---
+++ src/providers/facialrecognition_provider.js
@@ -0,0 +1,20 @@
+const getFacialRecognition = () => {
+    fetch('https://led-ls.co/recognitionimage', {
+        'method': 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            imagen: base64img
+        })
+    })
+        .then((response) => response.json())
+        .then((responseData) =>
+            this.setState({
+                contenido: responseData.contenido,
+                tipo: responseData.tipo_contenido,
+                valor: responseData.valor
+            })
+        )
+}
